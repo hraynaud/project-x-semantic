37da29b8aedbed73c9957a141064579938b761ae
hraynaud <herby.raynaud@gmail.com>
2016-10-16 20:15:06 -0700

    removes comments

--- client/index.js
+++ client/index.js
@@ -2,6 +2,4 @@ import React from 'react';
 import {render} from 'react-dom';
 import Main from './components/Main'; 
 
-// Render the main app react component into the app div.
-// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
 render(<Main />, document.getElementById('app'));
